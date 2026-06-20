eb29aeae569f62146e41eb8b85950a6baf5473e1
Alexandra Suslova <alexandra11622@yandex.ru>
2026-06-20 15:58:37 +0300

    удалила лишние строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,5 +1,4 @@
 function truncate(str, maxlength) {
-<<<<<<< HEAD
   return (str.length > maxlength)
     ? str.slice(0, maxlength - 1) + '…'
     : str;
@@ -7,7 +6,5 @@ function truncate(str, maxlength) {
 
 console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20) === 'Вот, что мне хотело…');
 console.log(truncate('Всем привет!', 20) === 'Всем привет!');
-=======
   // ваш код...
 }
->>>>>>> 13043f0f64687fccaf0f96369597d2a429be0573
